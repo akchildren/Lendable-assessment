12bece08838defc3ed6eff6fca1adc6da11fcaff
Adam Children <31509717+akchildren@users.noreply.github.com>
2025-09-09 14:07:11 +0100

    r: LoanTermBreakpoints to be stricter on immutability

--- src/Domain/Loan/LoanTermBreakpoints.php
+++ src/Domain/Loan/LoanTermBreakpoints.php
@@ -4,15 +4,20 @@ namespace Lendable\Interview\Domain\Loan;
 
 use Lendable\Interview\Exception\Loan\Term\LoanTermBreakpointsUndefinedException;
 
-final class LoanTermBreakpoints
+final readonly class LoanTermBreakpoints implements DomainModelInterface
 {
+    /**
+     * @var array<int, int>
+     */
+    public array $breakpoints;
+
     /**
      * @param array<int, int> $breakpoints [amount => fee]
      */
-    public function __construct(
-        private array $breakpoints
-    ) {
-        ksort($this->breakpoints);
+    public function __construct(array $breakpoints)
+    {
+        ksort($breakpoints);
+        $this->breakpoints = $breakpoints;
     }
 
     /**
