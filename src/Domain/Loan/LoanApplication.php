3e90c6e66fa97a302c4e17619c0d724294a9ed2c
Adam Children <31509717+akchildren@users.noreply.github.com>
2025-09-09 14:09:44 +0100

    Update LoanApplication.php

--- src/Domain/Loan/LoanApplication.php
+++ src/Domain/Loan/LoanApplication.php
@@ -26,7 +26,10 @@ final readonly class LoanApplication implements DomainModelInterface
     }
 
     /**
-     * @return array<string, Money|LoanTerm>
+     * Returns the domain object as an array
+     * Useful for serialization, logging, or testing.
+     *
+     * @return array<string, mixed> Associative array representation of the domain object.
      */
     public function toArray(): array
     {
