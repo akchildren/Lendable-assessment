1f04f66e9fff0117b6912b54b14cadb31fb38950
Adam Children <31509717+akchildren@users.noreply.github.com>
2025-09-09 14:09:19 +0100

    r: remove enforced toArray function from DomainModelInterface

--- src/Domain/DomainModelInterface.php
+++ src/Domain/DomainModelInterface.php
@@ -6,11 +6,4 @@ namespace Lendable\Interview\Domain;
 
 interface DomainModelInterface
 {
-    /**
-     * Returns the domain object as an array
-     * Useful for serialization, logging, or testing.
-     *
-     * @return array<string, mixed> Associative array representation of the domain object.
-     */
-    public function toArray(): array;
 }
